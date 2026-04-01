7a7156e4bb6a506ee353467a0dcf0341ec290e28
DemonDi666 <demondi6666@gmail.com>
2026-04-01 15:53:21 +0300

    Create class Unit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
+﻿
 
 namespace ConsoleApp1
 {
@@ -8,80 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            PrintFiboncci();
-            PrintEvenNumbers();
-            PrintMultiplacationTable();
-            GetPassword();
-        }
-
-        private static void PrintFiboncci()
-        {
-            int[] fibonacci = new int[10];
-
-            fibonacci[0] = 0;
-            fibonacci[1] = 1;
-
-            for (int i = 2; i < fibonacci.Length; i++)
-            {
-                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-            }
-
-            foreach (var count in fibonacci)
-            {
-                Console.Write(count + " ");
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void PrintEvenNumbers()
-        {
-
-            for (int i = 1; i < 21; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.Write(i + " ");
-                }
-            }
-            Console.WriteLine();
-        }
-
-        private static void PrintMultiplacationTable()
-        {
-            for (int i = 1; i <= 5; i++)
-            {
-                for (int j = 1; j <= 5; j++)
-                {
-                    Console.Write(i*j +" ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static void GetPassword()
-        {
-            string password = "qwerty";
-            string line;
-            int i = 0;
-            int result;
-
-            do
-            {
-                Console.Write("Enter password:");
-                line = Console.ReadLine();
-
-                result = string.Compare(line, password);
-
-                if (result != 0)
-                {
-                    Console.WriteLine("Incorrect Password!");
-                }
-
-                i++;
-            }
-            while (result!=0);
-            Console.WriteLine("Correct Password!");
+            
         }
     }
 }
---
+++ Unit.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal class Unit
+    {
+        private float _health;
+        
+        public float Armor { get; set; }
+        public string Name { get; }
+        public int Damage { get; }
+
+        public float Health => _health;
+
+        public Unit(int damage=5, float armor=0.6f)
+        {
+            Damage = damage;
+            Armor = armor;
+        }
+
+        public Unit() : this("Unknown Unit")
+        {
+        }
+
+        public Unit(string name) { 
+            Name = name;
+        }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(int damage)
+        {
+            _health = Health - damage * Armor;
+
+            if(Health <= 0f)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
